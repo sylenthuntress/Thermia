f18cb46807a4ef9b375c22e3657e92e0235f9e13
SylentHuntress <GaymerGirlEva@gmail.com>
2025-02-17 13:48:32 -0600

    Configure datagen

---
+++ src/main/java/sylenthuntress/thermia/data/ThermiaDataGenerator.java
@@ -0,0 +1,13 @@
+package sylenthuntress.thermia.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThermiaDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+
+
+}
