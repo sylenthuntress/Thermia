123b8eed7754002c39e64737c8fc6398352637c3
SylentHuntress <GaymerGirlEva@gmail.com>
2025-02-18 11:36:07 -0600

    Added a slight amount of randomization to the advancements screen

--- src/main/java/sylenthuntress/thermia/mixin/client/temperature/AdvancementTabMixin.java
+++ src/main/java/sylenthuntress/thermia/mixin/client/temperature/AdvancementTabMixin.java
@@ -6,8 +6,11 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.advancement.AdvancementTab;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.math.random.RandomSeed;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import sylenthuntress.thermia.Thermia;
 
@@ -17,6 +20,8 @@ import java.util.function.Function;
 public class AdvancementTabMixin {
     @Shadow
     private double originY;
+    @Unique
+    private final long seed = RandomSeed.getSeed();
 
     @WrapOperation(
             method = "render",
@@ -38,10 +43,18 @@ public class AdvancementTabMixin {
             int textureWidth,
             int textureHeight,
             Operation<Void> original) {
+        Random random = Random.create(seed * y * x);
+
         if (y > this.originY + 16) {
-            sprite = Thermia.modIdentifier("textures/advancements/background/magma_block.png");
+            sprite = Thermia.modIdentifier(
+                    "textures/advancements/background/magma_block_"
+                            + (random.nextInt(2) + 1)
+                            + ".png"
+            );
         } else if (y > this.originY) {
             sprite = Thermia.modIdentifier("textures/advancements/background/stitched.png");
+        } else if (y < this.originY + 32 && random.nextInt(2) == 0) {
+            sprite = Identifier.ofVanilla("textures/block/snow.png");
         }
 
         original.call(instance, renderLayers, sprite, x, y, u, v, width, height, textureWidth, textureHeight);
